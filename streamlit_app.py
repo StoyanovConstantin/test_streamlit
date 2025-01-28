9dd4277f81d27463d6643a6ca6b2ce325c110673
ITClubWeb <stoyanov.cons@gmail.com>
2025-01-28 12:17:29 +0300

    add file for read

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import csv
 
 
 st.title('My new project 2025')
+file = open(r"file.txt", 'r', encoding='UTF-8')
+filerow = csv.reader(file)
+for f in filerow:
+    st.write(f)
+file.close()
